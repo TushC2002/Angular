2cda61ec7282be9182795f59c4590b4ff4f5e0eb
name <chaudharitush313@gmail.com>
2024-03-13 10:39:09 +0530

    done

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  email: string = '';
+  password: string = '';
+
+  constructor(private router: Router) {}
+
+  login() {
+    const users = [
+      { id: 1, name: "Tushar Chaudhari", email: "admin@gmail.com", password: "admin123", role: "Admin" },
+      { id: 2, name: "Tushar Chaudhari", email: "superadmin@gmail.com", password: "superadmin123", role: "Super Admin" }
+    ];
+
+    const user = users.find(user => user.email === this.email && user.password === this.password);
+
+    if (user) {
+      this.router.navigate(['/home']);
+    } else {
+      alert("Invalid email or password");
+    }
+  }
+}
+
